dea1cba34c8839dd320c1e3356c13667608aed08
cxh <1319817447@qq.com>
2021-05-10 01:36:54 +0800

    房源详情

--- module_home/src/main/java/com/huihe/module_home/ui/fragment/HouseTakeLookRecordInsertFragment.kt
+++ module_home/src/main/java/com/huihe/module_home/ui/fragment/HouseTakeLookRecordInsertFragment.kt
@@ -1,6 +1,7 @@
 package com.huihe.module_home.ui.fragment
 
 import android.os.Bundle
+import android.text.Html
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +17,8 @@ import com.kotlin.base.ui.fragment.BaseMvpFragment
 import kotlinx.android.synthetic.main.fragment_house_take_look_record_insert.*
 import org.jetbrains.anko.support.v4.toast
 
-class HouseTakeLookRecordInsertFragment : BaseMvpFragment<HouseTakeLookRecordInsertPresenter>(),HouseTakeLookRecordInsertView{
+class HouseTakeLookRecordInsertFragment : BaseMvpFragment<HouseTakeLookRecordInsertPresenter>(),
+    HouseTakeLookRecordInsertView {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,20 +46,26 @@ class HouseTakeLookRecordInsertFragment : BaseMvpFragment<HouseTakeLookRecordIns
             // 选择房源
         }
         tvTakeLookInsert.onClick {
-            if (checkInput()){
+            if (checkInput()) {
                 mPresenter.addHouseTakeLookRecord(
                     evaluate = EtTakeLookInsertEvaluation.text.toString().trim()
                 )
             }
         }
+        var customer_evaluation = String.format(
+            "<font color=\"#ff0000\">%s</font>%s",
+            "*",
+            resources.getString(R.string.customer_evaluation)
+        )
+        tvTakeLookEvaluation.text = Html.fromHtml(customer_evaluation)
     }
 
     private fun checkInput(): Boolean {
-        if (TextUtils.isEmpty(nsvTakeLookInsert_house.getContent().toString().trim())){
+        if (TextUtils.isEmpty(nsvTakeLookInsert_house.getContent().toString().trim())) {
             toast(resources.getString(R.string.takelook_house_input))
             return false
         }
-        if (TextUtils.isEmpty(EtTakeLookInsertEvaluation.text.toString().trim())){
+        if (TextUtils.isEmpty(EtTakeLookInsertEvaluation.text.toString().trim())) {
             toast(resources.getString(R.string.customer_evaluation_input))
             return false
         }
